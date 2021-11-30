fa8bb6c2aa09e888e0eaee17c84b0abffa3ea793
Michael Brinkmann <29630837+wpsmmb@users.noreply.github.com>
2021-11-30 11:14:20 +0100

    Add files via upload

---
+++ DevTestData/BaseDalc.cs
@@ -0,0 +1,34 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+
+namespace Wescale.DevTestData {
+
+    /// <summary>
+    /// Basis Datanbank-Layer
+    /// </summary>
+    public abstract class BaseDalc {
+
+        private static ISessionFactory _sessionFactory;
+
+        static BaseDalc() {
+            // Initialize NHibernate
+            Configuration cfg = new Configuration();
+            cfg.Configure();
+            cfg.AddAssembly(typeof(DevTestCommon.Entity).Assembly);
+
+            SchemaExport schemaExport = new SchemaExport(cfg);
+            schemaExport.Execute(false, true, false);
+
+            _sessionFactory = cfg.BuildSessionFactory();
+        }
+
+        /// <summary>
+        /// Liefert eine konfigurierte <see cref="ISessionFactory"/>
+        /// </summary>
+        internal static ISessionFactory SessionFactory {
+            get => _sessionFactory;
+        }
+
+    }
+}
---
+++ DevTestData/UserDalc.cs
@@ -0,0 +1,65 @@
+﻿using NHibernate;
+using System;
+using System.Collections.Generic;
+using Wescale.DevTestCommon;
+
+namespace Wescale.DevTestData {
+
+    /// <summary>
+    /// Datanbank-Layer für Benutzer.
+    /// </summary>
+    public class UserDalc : BaseDalc {
+
+        /// <summary>
+        /// Load user.
+        /// </summary>
+        public User Get(int id) {
+            ISession session = SessionFactory.OpenSession();
+            return session.Get<User>(id);
+        }
+
+        /// <summary>
+        /// Save user.
+        /// </summary>
+        public int Save(User user) {
+            ISession session = SessionFactory.OpenSession();
+            if (user.Version > 0) {
+                user.ModifiedOnUtc = DateTime.UtcNow;
+            }
+            int id = (int)session.Save(user);
+            session.Flush();
+            return id;
+        }
+
+        /// <summary>
+        /// Save or update user.
+        /// </summary>
+        public void SaveOrUpdate(User user) {
+            ISession session = SessionFactory.OpenSession();
+            if (user.Version > 0) {
+                user.ModifiedOnUtc = DateTime.UtcNow;
+            }
+            session.SaveOrUpdate(user);
+            session.Flush();
+        }
+
+        /// <summary>
+		/// Retrieve <see cref="User"/> page wise.
+		/// </summary>
+        /// <param name="pageIndex">Index of page</param>
+        /// <param name="pageSize">Size of page</param>
+        /// <param name="totalRecords">Total count of users.</param>
+		public IList<User> List(int pageIndex, int pageSize, out int totalRecords) {
+            totalRecords = 42;
+
+            ISession session = SessionFactory.OpenSession();
+
+            ICriteria criteria = session.CreateCriteria<User>();
+            criteria.SetFirstResult(pageIndex);
+            criteria.SetMaxResults(pageSize);
+
+            return criteria.List<User>();
+        }
+
+    }
+}
